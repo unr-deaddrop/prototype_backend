b6a62aa4b7cff0c84891df43f8debc81bb368d8d
Your Name <name@domain.example>
2023-11-30 14:12:16 -0800

    up to date

--- backend/views.py
+++ backend/views.py
@@ -2,8 +2,19 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from backend.models import Agent
+from backend.serializers import AgentSerializer
 
 # Create your views here.
 @api_view(['GET'])
 def agents(request):
-    return Response({'test': 'data'})
+    agents = Agent.objects.all()
+    serializer = AgentSerializer(agents, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addAgent(request):
+    serializer = AgentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
