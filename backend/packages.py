a79753beefae651b4d8bad6754ff394bf9da2ea0
lgactna <lgonzalesna@gmail.com>
2024-03-01 12:20:19 -0800

    modify database fields to support the package manager

---
+++ backend/packages.py
@@ -0,0 +1,8 @@
+"""
+The DeadDrop package manager.
+
+Agents and protocol handlers come in .zip files that are in a standard format,
+allowing the server to discover various agent metadata and 
+"""
+
+from pathlib import Path
