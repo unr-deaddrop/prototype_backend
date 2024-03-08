6d50fbeb261f32d4dbffc302698dd344e8bd4031
lgactna <lgonzalesna@gmail.com>
2024-03-08 14:04:46 -0800

    Increase default page size

--- deaddrop/settings.py
+++ deaddrop/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['backend.filters.AllDjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 5
 }
 
 MIDDLEWARE = [
